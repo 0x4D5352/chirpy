1e2d816899931f599770140baa3f0a8429081f1c
Joshua Nussbaum <145164917+jnussbaumol@users.noreply.github.com>
2024-11-30 06:20:34 -0600

    commit to transfer

--- main.go
+++ main.go
@@ -1,16 +1,60 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	serv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", checkHealth)
+	mux.HandleFunc("/metrics", apiCfg.checkMetrics)
+	mux.HandleFunc("/reset", apiCfg.resetMetrics)
 	log.Fatal(serv.ListenAndServe())
+}
+
+func checkHealth(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	int, err := io.WriteString(w, "OK")
+	if err != nil {
+		log.Fatal(err, int)
+	}
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	handler := next
+	return handler
+}
+func (cfg *apiConfig) checkMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	int, err := io.WriteString(w, fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
+	if err != nil {
+		log.Fatal(err, int)
+	}
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Swap(0)
+	int, err := io.WriteString(w, "Metrics reset!")
+	if err != nil {
+		log.Fatal(err, int)
+	}
 }
