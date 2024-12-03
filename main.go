e7a5f9466755947e61dae2ddf3904e10281b3704
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-12-03 16:18:47 -0600

    finish routing and architecture modules

--- main.go
+++ main.go
@@ -20,10 +20,10 @@ func main() {
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	fmt.Println("Setting up health endpoint...")
-	mux.HandleFunc("/healthz", checkHealth)
+	mux.HandleFunc("GET /api/healthz", checkHealth)
 	fmt.Println("Setting up metrics endpoint...")
-	mux.HandleFunc("/metrics", apiCfg.checkMetrics)
-	mux.HandleFunc("/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.checkMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
 	fmt.Println("Starting Server...")
 	log.Fatal(serv.ListenAndServe())
 }
@@ -53,9 +53,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) checkMetrics(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Checking metrics...")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	int, err := io.WriteString(w, fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load()))
+	body := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileServerHits.Load())
+	int, err := io.WriteString(w, body)
 	if err != nil {
 		log.Fatal(err, int)
 	}
